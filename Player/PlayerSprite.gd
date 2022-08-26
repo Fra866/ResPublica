dc6584df42c3e105cee866c32b3fa874b12cb0cf
DankTourist <diracsea95@protonmail.com>
2022-08-26 09:16:14 +0200

    PlayerSprite: Simplified raycast

--- Player/PlayerSprite.gd
+++ Player/PlayerSprite.gd
@@ -2,7 +2,8 @@ extends KinematicBody2D
 const SPEED = 180.0
 
 onready var battle_box = get_parent().get_parent()
-onready var raycast = [$RayCast2D, $RayCast2D2, $RayCast2D3, $RayCast2D4]
+onready var raycast_top = $RayCast2D
+onready var raycast = [$RayCast2D2, $RayCast2D3, $RayCast2D4]
 var velocity = Vector2(0, 0)
 
 enum TURN {WAIT, FIGHT}
@@ -24,19 +25,19 @@ func _process(delta):
 		turn = TURN.WAIT
 		
 	if turn == TURN.FIGHT:
-		for rc in raycast:
-			if rc.is_colliding():
-				var col = rc.get_collider()
-				if "Bullet" in col.name:
-					emit_signal("hit", col.bullet_resource.damage)
-					battle_box.remove(col)
+#		for rc in raycast:
+		if raycast_top.is_colliding():
+			var col = raycast_top.get_collider()
+			if "Bullet" in col.name:
+				emit_signal("hit", col.bullet_resource.damage)
+				battle_box.remove(col)
 #					print("Hit")
 					
-		if Input.is_action_pressed("ui_down") and !raycast[0].is_colliding():
+		if Input.is_action_pressed("ui_down") and !raycast_top.is_colliding():
 			position.y += SPEED * delta
-		if Input.is_action_pressed("ui_left") and !raycast[1].is_colliding():
+		if Input.is_action_pressed("ui_left") and !raycast[0].is_colliding():
 			position.x -= SPEED * delta
-		if Input.is_action_pressed("ui_up") and !raycast[2].is_colliding():
+		if Input.is_action_pressed("ui_up") and !raycast[1].is_colliding():
 			position.y -= SPEED * delta
-		if Input.is_action_pressed("ui_right") and !raycast[3].is_colliding():
+		if Input.is_action_pressed("ui_right") and !raycast[2].is_colliding():
 			position.x += SPEED * delta
