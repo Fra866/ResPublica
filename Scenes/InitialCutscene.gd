a01206defddbd1bcdfb83d99de97a7546ed81e63
Fra866 <chicco.savalli@gmail.com>
2022-09-27 09:55:20 +0200

    InitialCutscene: Ask gender function

--- Scenes/InitialCutscene.gd
+++ Scenes/InitialCutscene.gd
@@ -5,16 +5,20 @@ export(Array, String) var list_texts = []
 onready var scene_manager = get_parent().get_parent()
 onready var ui = scene_manager.get_child(5)
 onready var menu = scene_manager.get_child(1)
+onready var name_label = menu.get_child(0).get_child(0).get_child(1).get_child(4)
 
 onready var sprite = $ImageContainer/Sprite
 onready var text_label = $TextContainer/RichTextLabel
 onready var input_name_edit = $AskForName/LineEdit
+onready var buttons_container = $AskForGender
+onready var buttons = [$AskForGender/Panel/Man, $AskForGender/Panel/Woman]
 
 enum STATE_CUTSCENE {INTRO, ASKING_NAME, ASKING_GENDER}
 onready var state = STATE_CUTSCENE.INTRO
 
 
 func _ready():
+	buttons_container.visible = false
 	input_name_edit.visible = false
 	ui.visibility(false)
 	yield(get_tree().create_timer(1), "timeout")
@@ -26,8 +30,8 @@ func animation():
 	for current_text in list_texts:
 		text_label.text = current_text
 		text_label.visible_characters = 0
-		
-		for _i in len(text_label.text):
+			
+		for letter in len(current_text):
 			yield(get_tree().create_timer(0.05), "timeout")
 			text_label.visible_characters += 1
 		
@@ -35,26 +39,50 @@ func animation():
 	
 	# menu.visible = true
 	
-	ask_name_and_gender()
+	ask_name()
 
 
-func ask_name_and_gender():
+func ask_name():
 	state = STATE_CUTSCENE.ASKING_NAME
 	sprite.visible = false
 	text_label.rect_position.y = -16
-	
 	text_label.text = "Come ti chiami?"
 	
+	for i in len(text_label.text):
+		text_label.visible_characters = i
 	
 	input_name_edit.visible = true
 	input_name_edit.grab_focus()
+	
 	# start_game()
 
 
+func ask_gender():
+	buttons_container.visible = true
+	state = STATE_CUTSCENE.ASKING_GENDER
+	
+	text_label.text = "Sei uomo o donna?"
+	
+	for i in len(text_label.text):
+		text_label.visible_characters = i
+	
+	buttons[0].grab_focus()
+
+
 func _process(_delta):
-	if state == STATE_CUTSCENE.ASKING_NAME:
-		if Input.is_action_just_pressed("ui_accept"):
-			scene_manager.save_file.name = input_name_edit.text
+	if Input.is_action_just_pressed("ui_accept"):
+		if state == STATE_CUTSCENE.ASKING_NAME:
+			name_label.text = input_name_edit.text
+			scene_manager.save_file.name = name_label.text
+			input_name_edit.visible = false
+			
+			ask_gender()
+		
+		elif state == STATE_CUTSCENE.ASKING_GENDER:
+			name_label.text = input_name_edit.text
+			scene_manager.save_file.name = name_label.text
+			input_name_edit.visible = false
+			
 			start_game()
 
 
